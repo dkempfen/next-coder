ce0252295f45c9c0884bb16796f6b8ee6ecdcfd1
dkempfen <107085437+dkempfen@users.noreply.github.com>
2025-01-22 21:00:47 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -17,12 +17,19 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "ecommerce-platform",
   description: "App de muestra en next JS",
+  author: "Dante Kempfen",
+  keywords: "ecommerce, nextjs, plataforma",
+  viewport: "width=device-width, initial-scale=1.0",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    
+      <head>
+        <meta name="author" content={metadata.author} />
+        <meta name="keywords" content={metadata.keywords} />
+        <meta name="viewport" content={metadata.viewport} />
+      </head>
       <body className="bg-background min-h-screen flex flex-col">
         <Header />
         <main className="grow p-4">
